2ad0497046183ce468262316af4555567867cdf2
Leticia <leticiamenezes77@gmail.com>
2022-08-17 22:29:10 -0300

    Arquivos de teste de aula commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', function(request, response){
+    response.render('Index')
+
+});
+app.get('/sobre', function(request, response){
+    response.render('sobre')
+
+});
+app.listen('80', function(){
+    console.log('Server online:80');
+});
